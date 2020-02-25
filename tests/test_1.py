50f9c08d832198dcf11edea0730f52c58ed553a6
matze-dd <45763831+matze-dd@users.noreply.github.com>
2020-02-25 11:53:36 +0100

    Update test_1.py

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,2 +1,23 @@
 import tex2txt
-assert 2 == 3
+
+latex = r"""
+Only few people\footnote{We use
+\textcolor{red}{redx colour.}}
+is lazy.
+"""
+
+res = r"""
+Only few people
+is lazy.
+
+
+
+We use
+redx colour.
+"""
+
+opts = tex2txt.Options(lang='en')
+plain, nums = tex2txt.tex2txt(latex, opts)
+
+assert plain == res
+
